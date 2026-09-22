8a3eb9904956cc13a91ae49f8c166adf8b0638ba
Isabelle Neves <ineves@umass.edu>
2026-09-22 18:48:43 -0400

    testing function for assignment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Server, start!");
+    let u_input = String::from("Up Down Left Right");
+    user_movement(u_input);
+    // println!("This user wants to move {u_input}");
+    // Uh oh! Can't use this line now. 
+    // Comment it out to compile the code properly.
+    
+    let status = 404;
+    status_code(status);
+    println!("Server status: still {status} over here!");
 }
+
+fn user_movement (user_input: String) {
+    println!("User wishes to move {user_input}");
+} 
+// user_input goes out of scope and drop is called.
+// space in heap is freed, making the original variable invalid
+
+fn status_code (code: i32) {
+    println!("Server status: {code}");
+} 
+// code goes out of scope. Nothing special happens.
